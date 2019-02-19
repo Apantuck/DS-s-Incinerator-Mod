93b163d820d3448e4edae1e0aa7550f1bcfb7585
Alex Pantuck <apantuck@gmail.com>
2019-02-19 00:11:53 -0500

    compressor entity definition

--- prototypes/entity/incinerator-entity.lua
+++ prototypes/entity/incinerator-entity.lua
@@ -5,18 +5,17 @@
 ------------------------------------------------------------
 -- Incinerator
 
-local incinerator = table.deepcopy(data.raw.furnace["steel-furnace"])
-incinerator.name = "incinerator"
-incinerator.type = "assembling-machine"
-incinerator.crafting_categories = {"compression"}
-incinerator.render_no_network_icon = false
-incinerator.ingredient_count = 1
-incinerator.fixed_recipe = "methane-gas"
-incinerator.energy_source = {
+local incinerator_entity = table.deepcopy(data.raw.furnace["steel-furnace"])
+incinerator_entity.name = "incinerator"
+incinerator_entity.type = "assembling-machine"
+incinerator_entity.crafting_categories = {"compression"}
+incinerator_entity.render_no_network_icon = false
+incinerator_entity.ingredient_count = 1
+incinerator_entity.fixed_recipe = "methane-gas"
+incinerator_entity.energy_source = {
         type = "burner",
         fuel_category = "chemical",
         effectivity = 1,
-        usage_priority = "secondary-input",
         emissions = 0.05,
         fuel_inventory_size = 1,
         smoke = {
@@ -29,17 +28,36 @@ incinerator.energy_source = {
                 }
         }
 }
-incinerator.result_inventory_size = nil
-incinerator.source_inventory_size = nil
-incinerator.minable = {mining_time = 1, result = "incinerator"}
+incinerator_entity.result_inventory_size = nil
+incinerator_entity.source_inventory_size = nil
+incinerator_entity.minable = {mining_time = 1, result = "incinerator"}
 
 ------------------------------------------------------------
 -- Compressor
 
-local compressor_entity = {
-        type = "furnace",
-        name = "compressor",
-
+local compressor_entity = table.deepcopy(data.raw.furnace["steel-furnace"])
+compressor_entity.name = "compressor"
+compressor_entity.crafting_categories = {"compression"}
+compressor_entity.result_inventory_size = 1
+compressor_entity.source_inventory_size = 0
+compressor_entity.energy_source = {
+        type = "burner",
+        effectivity = 1,
+        emissions = 0.005,
+        fuel_inventory_size = 1,
+        fuel_categories = {"compressable", "chemical", "nuclear"}
+}
+compressor_entity.fluid_boxes = {
+      {
+        production_type = "output",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = 1,
+        pipe_connections = {{ type="output", position = {0, 2} }},
+        secondary_draw_orders = { north = -1 }
+      },
+      off_when_no_fluid_recipe = false
 }
 
 -----------------------------------------------------------
@@ -52,4 +70,9 @@ local compressor_entity = {
 
 
 ---------------------------------------------------------
-data:extend{incinerator}
+data:extend(
+        {
+                incinerator_entity,
+                compressor_entity
+        }
+)
